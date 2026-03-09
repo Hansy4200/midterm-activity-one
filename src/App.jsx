4d0b313056f1722a7a5db5252ea31c7d8bb258b3
paloga_h <paloga_h@ca01.ub.lan>
2026-03-09 12:02:09 +0800

    Configured the react-router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
-import Home from './Home';
-import About from './About';
 
 function App() {
   return (
     <BrowserRouter> {}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+
       </Routes>
     </BrowserRouter>
   );
